66ef8c61a429bec49b2642cb79277cb7e273e556
bfreiheit <belinda.freiheit@gmail.com>
2025-07-31 13:26:24 +0200

    improve upload

--- scripts/upload_gsc.py
+++ scripts/upload_gsc.py
@@ -4,23 +4,17 @@ import os
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(PROJECT_ROOT)
 
+DATA_DIR = os.path.join(PROJECT_ROOT, "data", "raw", "jaffle-data")
+
 from google.cloud import storage
 import config.config
 
 client = storage.Client()
-for bucket in client.list_buckets():
-    print(bucket.name)
+bucket = client.bucket("jaffle-files")
 
-
-def upload_blob(bucket_name, source_file_name, destination_blob_name):
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(destination_blob_name)
-    blob.upload_from_filename(source_file_name)
-    print(f"{source_file_name} uploaded to {destination_blob_name}.")
-
-upload_blob(
-    "my-data-lake-bucket",         # Bucketname
-    "local_file.csv",              # Lokale Datei
-    "raw/local_file.csv"           # Zielpfad in GCS
-)
+for file_name in os.listdir(DATA_DIR):
+    local_path = os.path.join(DATA_DIR, file_name)
+    if os.path.isfile(local_path):  # exclude directories
+        blob = bucket.blob(f"raw/{file_name}")
+        blob.upload_from_filename(local_path)
+        print(f"uploaded: {file_name}")
