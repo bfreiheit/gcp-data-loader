89d4fe2f012faf50467cd074aaf1bcd6515aae76
bfreiheit <belinda.freiheit@gmail.com>
2025-07-31 13:24:33 +0200

    add checks

--- config/config.py
+++ config/config.py
@@ -1,6 +1,17 @@
 import os
+import sys
 
 # path to GCP-credentials
 SERVICE_ACCOUNT_PATH = os.path.join("..", ".gcp", "gcs-user-key.json")
+
+# check if file exists
+if not os.path.exists(SERVICE_ACCOUNT_PATH):
+    sys.stderr.write(f"credentials not found: {SERVICE_ACCOUNT_PATH}\n")
+    sys.exit(1)
+
+# check if file is readable
+if not os.access(SERVICE_ACCOUNT_PATH, os.R_OK):
+    sys.stderr.write(f"no reading permission: {SERVICE_ACCOUNT_PATH}\n")
+    sys.exit(1)
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = SERVICE_ACCOUNT_PATH
